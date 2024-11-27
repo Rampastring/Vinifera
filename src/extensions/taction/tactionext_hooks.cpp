6f6be981499fdc2303ea089d9f641939598774f0
Rampastring <rampastring@outlook.com>
2024-11-27 04:11:30 +0200

    Use CDifficulty instead of Difficulty when enabling difficulty-specific triggers, ignore trigger difficulty flags in multiplayer

--- src/extensions/taction/tactionext_hooks.cpp
+++ src/extensions/taction/tactionext_hooks.cpp
@@ -151,10 +151,14 @@ DECLARE_PATCH(_TActionClass_Operator_Enable_Trigger_For_Difficulty_Patch)
         /**
          *  Set this trigger to be disabled if it is marked as disabled
          *  for this current mission difficulty.
+         *
+         *  Rampastring: Check CDifficulty instead of Difficulty.
+         *  Also, in non-campaign games, consider all difficulties enabled regardless of what the trigger specifies.
          */
-        if (Scen->Difficulty == DIFF_EASY && !trigger->Class->Easy
-            || Scen->Difficulty == DIFF_NORMAL && !trigger->Class->Normal
-            || Scen->Difficulty == DIFF_HARD && !trigger->Class->Hard) {
+        if (Session.Type == GAME_NORMAL &&
+            (Scen->CDifficulty == DIFF_HARD && !trigger->Class->Easy
+            || Scen->CDifficulty == DIFF_NORMAL && !trigger->Class->Normal
+            || Scen->CDifficulty == DIFF_EASY && !trigger->Class->Hard)) {
 
             trigger->Disable();
 
