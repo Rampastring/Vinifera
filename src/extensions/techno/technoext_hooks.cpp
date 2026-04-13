075c0d8b8e2b7bd4626edde23f1d183a257a1ca7
Rampastring <rampastring@outlook.com>
2026-04-13 18:50:42 +0300

    Fix brightness of Iron Curtain effect on dark maps

--- src/extensions/techno/technoext_hooks.cpp
+++ src/extensions/techno/technoext_hooks.cpp
@@ -3386,9 +3386,12 @@ int TechnoClassExt::_Apparent_Brightness(int brightness) const
     
     if (technoext->IronCurtainTimer > 0)
     {
+        float scale = Map[Center_Coord()].TileBrightness / 1000.0f;
+
         int index = (technoext->IronCurtainTimer.Value() / RuleExtension->IronCurtainFlashRate) % RuleExtension->IronCurtainPulseTable.Count();
         int extra = RuleExtension->IronCurtainPulseTable[index] * RuleExtension->IronCurtainFlashIntensityMultiplier;
-        int total = brightness + extra;
+        int extrascaled = (int)(extra * scale);
+        int total = brightness + extrascaled;
         if (total < 0)
             total = 0;
     
