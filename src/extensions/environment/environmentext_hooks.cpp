4b4932f960fa00ba1bc59d4696461e4bfbb8db7f
Rampastring <rampastring@outlook.com>
2026-06-03 00:33:38 +0300

    Restore environment hooks

--- src/extensions/environment/environmentext_hooks.cpp
+++ src/extensions/environment/environmentext_hooks.cpp
@@ -22,12 +22,12 @@
  */
 void EnvironmentExtension_Hooks()
 {
-    // Patch_Jump(0x004938A0, &ExtEnvironmentClass::Snapshot_Game_State);
-    // Patch_Jump(0x00493920, &ExtEnvironmentClass::Apply_To_Game_State);
-    // Patch_Jump(0x00493A30, &ExtEnvironmentClass::Load);
-    // Patch_Jump(0x00493A50, &ExtEnvironmentClass::Save);
-    // Patch_Jump(0x00493860, &ExtEnvironmentClass::Hook_Ctor);
-    // Patch_Jump(0x00493890, &ExtEnvironmentClass::Hook_Dtor);
-    // Patch_Jump(0x00493810, &ExtEnvironmentClass::Static_Init);
-    // Patch_Jump(0x00493850, &ExtEnvironmentClass::Static_Deinit);
+    Patch_Jump(0x004938A0, &ExtEnvironmentClass::Snapshot_Game_State);
+    Patch_Jump(0x00493920, &ExtEnvironmentClass::Apply_To_Game_State);
+    Patch_Jump(0x00493A30, &ExtEnvironmentClass::Load);
+    Patch_Jump(0x00493A50, &ExtEnvironmentClass::Save);
+    Patch_Jump(0x00493860, &ExtEnvironmentClass::Hook_Ctor);
+    Patch_Jump(0x00493890, &ExtEnvironmentClass::Hook_Dtor);
+    Patch_Jump(0x00493810, &ExtEnvironmentClass::Static_Init);
+    Patch_Jump(0x00493850, &ExtEnvironmentClass::Static_Deinit);
 }
